7b9b4ed6a6e5ae192e3aa5d23b3c1fcb9238fbfa
root <18955993726@163.com>
2018-12-07 12:48:25 +0800

    first commit

---
+++ ctp/CMakeFiles/vnctpmd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vnctpmd.dir/vnctpmd/vnctpmd/vnctpmd.cpp.o"
+  "lib/vnctpmd.pdb"
+  "lib/vnctpmd.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vnctpmd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ctp/CMakeFiles/vnctptd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vnctptd.dir/vnctptd/vnctptd/vnctptd.cpp.o"
+  "lib/vnctptd.pdb"
+  "lib/vnctptd.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vnctptd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
